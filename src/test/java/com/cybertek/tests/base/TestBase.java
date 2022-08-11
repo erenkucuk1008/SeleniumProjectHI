43348f52a8f725a7c001d8a0017cffb75d2f06c0
erenkucuk <erenkkucuk89@gmail.com>
2022-08-11 02:21:53 -1000

    added new packages and classes

--- src/test/java/com/cybertek/tests/base/TestBase.java
+++ src/test/java/com/cybertek/tests/base/TestBase.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
@@ -12,9 +15,13 @@ public abstract class TestBase {
     public WebDriver driver;
 
     @BeforeMethod
-    public void setUpMethod() throws InterruptedException {
+    public void setUpMethod() throws InterruptedException, IOException {
 
-        driver = WebDriverFactory.getDriver("chrome");
+        Properties properties = new Properties();
+        FileInputStream file = new FileInputStream("configuration.properties");
+        properties.load(file);
+        String browser = properties.getProperty("browser");
+        driver = WebDriverFactory.getDriver(browser);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
