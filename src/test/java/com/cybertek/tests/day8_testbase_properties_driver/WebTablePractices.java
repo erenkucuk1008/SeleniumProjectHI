692e857fd7be37bb8fb7cbb64841e1f471014eaf
erenkucuk <erenkkucuk89@gmail.com>
2022-08-11 00:04:05 -1000

    added new packages and classes

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -1,4 +1,32 @@
 package com.cybertek.tests.day8_testbase_properties_driver;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class WebTablePractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() throws InterruptedException {
+        //Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+
+        //Go to website:
+        //http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void verify_order_test(){
+
+
+    }
 }
