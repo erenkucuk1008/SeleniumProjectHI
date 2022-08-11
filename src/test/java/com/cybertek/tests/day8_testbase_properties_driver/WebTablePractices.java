b1a580110c3c36430d73757499feb7c3f0e9ea5f
erenkucuk <erenkkucuk89@gmail.com>
2022-08-11 00:48:43 -1000

    added new packages and classes

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -1,33 +1,18 @@
 package com.cybertek.tests.day8_testbase_properties_driver;
 
-import com.cybertek.utilities.WebDriverFactory;
+import com.cybertek.tests.base.TestBase;
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebOrderUtilities;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class WebTablePractices {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUpMethod() throws InterruptedException {
-
-        driver = WebDriverFactory.getDriver("chrome");
-
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
+public class WebTablePractices extends TestBase {
 
     @Test
     public void verify_order_test(){
 
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         WebOrderUtilities.logIntoSmartBear(driver);
-
+        BrowserUtils.sleep(1);
         WebOrderUtilities.verifyOrder(driver, "Mark Smith");
 
     }
