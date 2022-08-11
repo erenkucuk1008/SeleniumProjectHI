9d6eb7a6471479a31cbb4a1f4ad9e97a3ffc2a84
erenkucuk <erenkkucuk89@gmail.com>
2022-08-11 02:29:29 -1000

    added new packages and classes

--- src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/WebOrder_Practices.java
+++ src/test/java/com/cybertek/tests/day6_dropdown_review_javafaker/WebOrder_Practices.java
@@ -13,6 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +22,7 @@ public class WebOrder_Practices {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUpMethod() throws InterruptedException {
+    public void setUpMethod() throws InterruptedException, IOException {
         //Open browser
         driver = WebDriverFactory.getDriver("chrome");
 
