cb7258648ef61d82f54411a5c938a82763b0eed8
erenkucuk <erenkkucuk89@gmail.com>
2022-08-11 00:00:47 -1000

    added new packages and classes

--- src/test/java/com/cybertek/utilities/WebOrderUtilities.java
+++ src/test/java/com/cybertek/utilities/WebOrderUtilities.java
@@ -3,6 +3,9 @@ package com.cybertek.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class WebOrderUtilities {
 
@@ -21,8 +24,21 @@ public class WebOrderUtilities {
         logInButton.click();
     }
 
-    public static void verifyOrder(WebDriver driver, String name){
+    public static void verifyOrder(WebDriver driver, String expectedName){
+        //Listede verilen isim var mi, yok mu diye verify edecek methodu yaziyoruz
+            //table[@id='ctl00_MainContent_orderGrid']//tr//td[2] ---- ISMI VEREN WEB ELEMENT
 
-        
+        List<WebElement> allNames = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr//td[2]"));
+
+        for (WebElement eachName : allNames) {
+            if (eachName.getText().equals(expectedName)){
+                Assert.assertTrue(eachName.getText().equals(expectedName));
+                return;
+            }
+        }
+
+        //The only condition where the Assert.fail() line below executed is if 'expectedName' is not in the table
+        Assert.fail("The expectedName is not in the table.");
+
     }
 }
