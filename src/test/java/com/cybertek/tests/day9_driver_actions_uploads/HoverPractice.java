c4c9f2fddca1ff8281ba3b8c44804acd4446a7e3
erenkucuk <erenkkucuk89@gmail.com>
2022-08-14 02:24:31 -1000

    added new packages and classes

--- src/test/java/com/cybertek/tests/day9_driver_actions_uploads/HoverPractice.java
+++ src/test/java/com/cybertek/tests/day9_driver_actions_uploads/HoverPractice.java
@@ -1,4 +1,38 @@
 package com.cybertek.tests.day9_driver_actions_uploads;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class HoverPractice {
+
+    @Test
+    public void hover_test(){
+        Driver.getDriver().get("http://practice.cybertekschool.com/hovers");
+
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement img2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement img3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
+        WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
+        WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
+
+        //We must create object of Actions class object
+        Actions actions = new Actions(Driver.getDriver());
+
+        //hover over
+        actions.moveToElement(img1).perform();
+        //assert
+        Assert.assertTrue(user1.isDisplayed());
+
+        actions.moveToElement(img2).perform();
+        Assert.assertTrue(user2.isDisplayed());
+
+        actions.moveToElement(img3).perform();
+        Assert.assertTrue(user3.isDisplayed());
+    }
 }
