afe3c752f14f6a11f0601b00ea95aebc952fdf0f
erenkucuk <erenkkucuk89@gmail.com>
2022-08-15 02:48:08 -1000

    day10

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -1,4 +1,26 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id="ctl00_MainContent_username")
+    public WebElement inputUsername;
+
+    @FindBy(id="ctl00_MainContent_password")
+    public WebElement inputPassword;
+
+    @FindBy(id="id=\"ctl00_MainContent_login_button\"")
+    public WebElement loginButton;
+
+    @FindBy(id="ctl00_MainContent_status")
+    public WebElement errorMessage;
 }
