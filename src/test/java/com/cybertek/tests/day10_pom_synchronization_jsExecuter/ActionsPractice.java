c4a38505597f43d6439fd1220420b2c5b53ce4e8
erenkucuk <erenkkucuk89@gmail.com>
2022-08-15 00:01:37 -1000

    day10

---
+++ src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecuter/ActionsPractice.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.day10_pom_synchronization_jsExecuter;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractice {
+
+    @Test
+    public void p2_double_click_test(){
+
+    }
+}
